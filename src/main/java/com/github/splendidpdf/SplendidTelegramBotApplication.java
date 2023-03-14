104a2944f91d000da95f601cec43dbe54d308dc4
Farkhad <askarovfarkhad.96@gmail.com>
2023-03-14 22:06:10 +0300

    initial commit

---
+++ src/main/java/com/github/splendidpdf/SplendidTelegramBotApplication.java
@@ -0,0 +1,12 @@
+package com.github.splendidpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplendidTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplendidTelegramBotApplication.class, args);
+    }
+}
